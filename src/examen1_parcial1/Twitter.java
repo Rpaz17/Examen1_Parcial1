3cf481cb6090a3d3123267648f0c55e91a13d28c
Azalia <Azalia@LAPTOP-9CLCUKCA>
2023-08-18 14:20:00 -0600

    Twitter

--- src/examen1_parcial1/Twitter.java
+++ src/examen1_parcial1/Twitter.java
@@ -5,6 +5,17 @@ package examen1_parcial1;
  *
  * @author fampa
  */
-public class Twitter {
+public class Twitter extends SocialClass{
+    
+    public Twitter(String username){
+        super(username);
+    }
+    
+    @Override
+    public void timeline(){
+        for(String posts1:posts){
+            System.out.println(posts1);   
+        }
+    }
     
 }
