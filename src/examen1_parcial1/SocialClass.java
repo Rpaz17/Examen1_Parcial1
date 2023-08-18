15cc67f552aea472eb872fc1cdca1ac30cb361cc
Daniel Cruz <danielcruzp@unitec.edu>
2023-08-18 15:19:40 -0600

    Cambios

--- src/examen1_parcial1/SocialClass.java
+++ src/examen1_parcial1/SocialClass.java
@@ -8,58 +8,54 @@ import javax.swing.JOptionPane;
  * @author fampa
  */
 public abstract class SocialClass {
-    
-    public ArrayList<String>friends;
-    public ArrayList<String>posts;
+
+    public ArrayList<String> friends;
+    public ArrayList<String> posts;
     public String username;
     public int contadorAmigos;
-    
-    public SocialClass(String username){
-        friends=new ArrayList<>(); 
-        posts=new ArrayList<>();
-        this.username=username;
+
+    public SocialClass(String username) {
+        friends = new ArrayList<>();
+        posts = new ArrayList<>();
+        this.username = username;
     }
-    
-      //Agregar Friend
-    public boolean addFriend(String user){
-        if(!user.equals(username)&&!friends.contains(user)){
-           friends.add(user);
-           return true;
+
+    //Agregar Friend
+    public boolean addFriend(String user) {
+        if (!user.equals(getUsername()) && !friends.contains(user)) {
+            friends.add(user);
+            return true;
         }
         return false;
     }
-    
-    public void addPost(String msg){
+
+    public void addPost(String msg) {
         posts.add(msg);
     }
-    
+
     public abstract void timeline();
-   
-    public void myProfile(){
-        JOptionPane.showMessageDialog(null, "Username: "+username, "USER",JOptionPane.INFORMATION_MESSAGE);
-        System.out.println("Username: "+username);
+
+    public void myProfile() {
+        String seimprimira;
+        seimprimira = "Username: " + username;
         timeline();
-        contadorAmigos=0;
-      
-        for(String friends1:friends){
-            System.out.println(friends1+(" "));
-            contadorAmigos=contadorAmigos+1;
-            if(contadorAmigos>=10){
-                System.out.println("\n"); 
-                contadorAmigos=0;
+        contadorAmigos = 0;
+
+        for (String friends1 : friends) {
+            seimprimira = seimprimira + friends1 + " ";
+            contadorAmigos = contadorAmigos + 1;
+            if (contadorAmigos >= 10) {
+                System.out.println("\n");
+                contadorAmigos = 0;
             }
-            
+
         }
-        
+        JOptionPane.showMessageDialog(null, seimprimira);
+
     }
 
     public String getUsername() {
         return username;
     }
-    
-    
-    
-    
-    
-    
+
 }
