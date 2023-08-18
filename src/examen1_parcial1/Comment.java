a91addf698062dc431985f91c46f9dd88d9392c0
Daniel Cruz <danielcruzp@unitec.edu>
2023-08-18 13:34:26 -0600

    Comment

--- src/examen1_parcial1/Comment.java
+++ src/examen1_parcial1/Comment.java
@@ -1,10 +1,29 @@
 
 package examen1_parcial1;
 
-/**
- *
- * @author fampa
- */
-public class Comment {
+import java.util.Calendar;
+
+public final class Comment {
+    
+    int postId;
+    String autor;
+    String contenidoComentario;
+    Calendar fecha=Calendar.getInstance();
+
+    public Comment(int postId, String autor, String contenidoComentario) {
+        this.postId = postId;
+        this.autor = autor;
+        this.contenidoComentario = contenidoComentario;
+    }
+    
+    public String print(){
+        String imprimir = autor+" - "+fecha
+                +"\n"+contenidoComentario;
+        
+        return imprimir;
+        
+    }
+    
+    
     
 }
