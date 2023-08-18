3294db51bdf696241842bcadb6eb268c8d6f0b7c
Azalia <Azalia@LAPTOP-9CLCUKCA>
2023-08-18 14:09:02 -0600

    social class

--- src/examen1_parcial1/SocialClass.java
+++ src/examen1_parcial1/SocialClass.java
@@ -1,9 +1,54 @@
 package examen1_parcial1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author fampa
  */
-public class SocialClass {
+public abstract class SocialClass {
+    
+    public ArrayList<String>friends;
+    public ArrayList<String>posts;
+    public String username;
+    public int contadorAmigos;
+    
+    public SocialClass(String username){
+        friends=new ArrayList<>(); 
+        posts=new ArrayList<>();
+        this.username=username;
+    }
+    
+      //Agregar Friend
+    public boolean addFriend(String user){
+        if(!user.equals(username)&&!friends.contains(user)){
+           friends.add(user);
+           return true;
+        }
+        return false;
+    }
+    
+    public void addPost(String msg){
+        posts.add(msg);
+    }
+    
+    public abstract void timeline();
+   
+    public void myProfile(){
+        System.out.println("Username: "+username);
+        timeline();
+        contadorAmigos=0;
+      
+        for(String friends1:friends){
+            System.out.println(friends1+(" "));
+            contadorAmigos=contadorAmigos+1;
+            if(contadorAmigos>=10){
+                System.out.println("\n"); 
+                contadorAmigos=0;
+            }
+            
+        }
+        
+    }
     
 }
